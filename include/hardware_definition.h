a491fa7438f8b2f5370e0d3850907a2f94ba4fd4
wanysteus <20403813+wanysteus@users.noreply.github.com>
2020-07-19 17:28:25 +0200

    add pin definition

---
+++ include/hardware_definition.h
@@ -0,0 +1,26 @@
+/* Pinout */
+#define PIN_RFM_RESET 9
+#define PIN_RFM_NSS 10
+#define PIN_MOSI 11
+#define PIN_MISO 12
+#define PIN_SCK 13
+#define PIN_RFM_INT 14
+#define PIN_LED_CTRL 17
+#define PIN_PWM1 16
+#define PIN_PWM2 20
+#define PIN_PWM3 22
+#define PIN_IO1 1
+#define PIN_IO2 A11
+#define PIN_IO3 A10
+#define PIN_IO4 23
+#define PIN_IN1_B1 7
+#define PIN_IN2_B1 8
+#define PIN_IN1_B2 5
+#define PIN_IN2_B2 6
+#define PIN_IN1_B3 3
+#define PIN_IN2_B3 4
+#define PIN_BAT1 19
+#define PIN_BAT2 18
+
+/* Other definitions */
+#define NUM_RGB_LEDS 1
