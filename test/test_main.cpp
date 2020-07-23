b05d3886b8e87f55c9d8ded1ddde20585ff7fd75
wanysteus <20403813+wanysteus@users.noreply.github.com>
2020-07-23 11:59:30 +0200

    add unit test for servos

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,7 @@
 #include "test_main.h"
 
 #include <Adafruit_NeoPixel.h>
+#include <PWMServo.h>
 #include <RH_RF95.h>
 #include <SPI.h>
 #include <unity.h>
@@ -10,6 +11,8 @@
 RH_RF95 rfm(PIN_RFM_NSS, digitalPinToInterrupt(PIN_RFM_INT));
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_RGB_LEDS, PIN_LED_CTRL, NEO_GRB + NEO_KHZ400);
+
+PWMServo servo1, servo2, servo3;
 
 void setup() {
   delay(2000);
@@ -24,6 +27,9 @@ void setup() {
 
   initOutputs();
   RUN_TEST(test_outputs);
+
+  initServos();
+  RUN_TEST(test_servos);
 
   UNITY_END();
 }
@@ -64,18 +70,47 @@ void test_rgb() {
 void test_outputs() {
   TEST_MESSAGE("Testing the main outputs, do you see them activate/deactivate ?");
   delay(1000);
+  TEST_MESSAGE("P1");
   digitalWrite(PIN_OUTPUT1, HIGH);
   delay(1000);
   digitalWrite(PIN_OUTPUT1, LOW);
+  TEST_MESSAGE("P2");
   digitalWrite(PIN_OUTPUT2, HIGH);
   delay(1000);
   digitalWrite(PIN_OUTPUT2, LOW);
+  TEST_MESSAGE("P3");
   digitalWrite(PIN_OUTPUT3, HIGH);
   delay(1000);
   digitalWrite(PIN_OUTPUT3, LOW);
+  TEST_MESSAGE("P4");
   digitalWrite(PIN_OUTPUT4, HIGH);
   delay(1000);
   digitalWrite(PIN_OUTPUT4, LOW);
+
+  TEST_PASS_MESSAGE("Test passed if saw it");
+}
+
+void test_servos() {
+  TEST_MESSAGE("Testing the servo outputs, do you see the servos move ?");
+  delay(1000);
+  TEST_MESSAGE("P5");
+  servo1.write(0);
+  delay(1000);
+  servo1.write(180);
+  delay(1000);
+  servo1.write(90);
+  TEST_MESSAGE("P6");
+  servo2.write(0);
+  delay(1000);
+  servo2.write(180);
+  delay(1000);
+  servo2.write(90);
+  TEST_MESSAGE("P7");
+  servo3.write(0);
+  delay(1000);
+  servo3.write(180);
+  delay(1000);
+  servo3.write(90);
 
   TEST_PASS_MESSAGE("Test passed if saw it");
 }
@@ -110,4 +145,10 @@ void initOutputs() {
   digitalWrite(PIN_OUTPUT2, LOW);
   digitalWrite(PIN_OUTPUT3, LOW);
   digitalWrite(PIN_OUTPUT4, LOW);
+}
+
+void initServos() {
+  servo1.attach(PIN_PWM1, SERVO_MIN_PULSE_WIDTH, SERVO_MAX_PULSE_WIDTH);
+  servo2.attach(PIN_PWM2, SERVO_MIN_PULSE_WIDTH, SERVO_MAX_PULSE_WIDTH);
+  servo3.attach(PIN_PWM3, SERVO_MIN_PULSE_WIDTH, SERVO_MAX_PULSE_WIDTH);
 }
--- test/test_main.h
+++ test/test_main.h
@@ -2,8 +2,10 @@
 void test_RFM();
 void test_rgb();
 void test_outputs();
+void test_servos();
 
 /* Utils */
 void initRFM();
 void initRGB();
 void initOutputs();
+void initServos();
