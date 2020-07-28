9c3d41c03f8ebed0fbfaf80d96421d614464646f
wanysteus <20403813+wanysteus@users.noreply.github.com>
2020-07-28 18:43:15 +0200

    add servo angle return

--- src/controller.h
+++ src/controller.h
@@ -10,8 +10,6 @@ void initCommunications();
 void showStatus();
 
 uint8_t getCommand(char* data);
-void sendPayload(uint8_t payload[]);
 void sendState();
 
 void toggleOutput(uint8_t pin, uint8_t en);
-void moveServo(PWMServo& servo, uint8_t angle);
